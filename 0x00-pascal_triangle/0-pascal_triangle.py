71f76942f71a4f0e47f5fae7278d8ecc95a91443
MarkMusili <musiliyrn@gmail.com>
2024-03-08 00:01:30 +0300

    Diffrent approach

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,16 +6,20 @@ Pascal's Triangle
 
 def pascal_triangle(n):
     """
-    Returns the Pascal's triangle of n
+    Returns Pascal's triangle of n rows
     Args:
         n (int): number of rows
     """
     if n <= 0:
         return []
 
-    triangle = [[1] * (i + 1) for i in range(n)]
-    for i in range(2, n):
-        for j in range(1, i):
-            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        triangle.append(row)
+        if i >= 2:
+            for j in range(1, i):
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
 
     return triangle
+
