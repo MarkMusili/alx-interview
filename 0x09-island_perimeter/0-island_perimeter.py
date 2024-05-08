f28536f65fb14d9c3504ba37caa69e68f34707f9
MarkMusili <musiliyrn@gmail.com>
2024-05-08 18:42:33 +0300

    Calculate the perimeter of an Island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Calculate the perimeter of a island
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of a 2 matrix
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for e in range(len(grid[i])):
+            if grid[i][e] == 1:
+                if i == 0 or grid[i - 1][e] == 0:
+                    perimeter += 1
+                if i == len(grid) or grid[i + 1][e] == 0:
+                    perimeter += 1
+                if e == 0 or grid[i][e - 1] == 0:
+                    perimeter += 1
+                if e == len(grid[i]) or grid[i][e + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
